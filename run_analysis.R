85439e6482cf4807cfa0b890dc9fbdf7abb87d93
andreaprb <andreabrito@live.com>
2015-07-25 22:28:55 -0700

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -13,9 +13,12 @@ packagesRequired<-function()
 
 downloadFiles<-function()
 {
+    # Create a directory in your working directory called data
     if(!file.exists("./data")){dir.create("./data")}
+    # Download the zip file which contains the data set
     fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip" 
     download.file(fileUrl,destfile="./data/dataset.zip", method="libcurl")
+    # Extract the files
     unzip ("./data/dataset.zip", exdir = "./data")
 }
 
